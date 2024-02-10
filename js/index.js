397aceac80564d861f4ec43dec69cd658e5e51f4
Alejandro <66854865+Alejandro-coder17@users.noreply.github.com>
2024-02-10 15:10:52 -0500

    Creando carreteras

--- js/index.js
+++ js/index.js
@@ -73,6 +73,7 @@ const graphInfo = graphString ? JSON.parse(graphString) : null;
 const graph = graphInfo ? 
             Graph.load(graphInfo)
             : new Graph();
+const world = new World(graph);
 const viewport = new Viewport(myCanvas);
 const graphEditor = new GraphEditor(viewport, graph);
 
@@ -80,6 +81,9 @@ animate();
 
 function animate() {
     viewport.reset();
+    world.generate();
+    world.draw(ctx);
+    ctx.globalAlpha = 0.3;
     graphEditor.display();
     requestAnimationFrame(animate);
 }
